1159943db1bfe345dff09baf501f1b8177c4f179
raul <raultc@macbook-pro-de-r.local>
2016-03-28 22:01:35 -0600

    Initial Commit

---
+++ where am i/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  where am i
+//
+//  Created by Raúl T on 28/03/16.
+//  Copyright © 2016 rtc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
